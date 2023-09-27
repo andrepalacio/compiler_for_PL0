e89406f371547a5a060d626831adf9025f2c4261
Andres Palacio <andres.palacio@utp.edu.co>
2023-09-27 08:38:22 -0500

    lexer fixed

--- lexer.py
+++ lexer.py
@@ -1,32 +1,18 @@
 from sly import Lexer
+from rich.table import Table
+from rich.console import Console
 
 class LexerForPL0(Lexer):
 
   # tokens
   tokens = [
-    FUN, BEGIN, END, IF, THEN, ELSE, WHILE, DO, VAR, ASSIGN, PRINT, 
+    FUN, BEGIN, END, IF, THEN, ELSE, WHILE, DO, VARDECL, ASSIGN, PRINT, 
     READ, WRITE, RETURN, PLUS, MINUS, TIMES, DIVIDE, EQ, NEQ, LT, GT,
-    LTE, GTE, SKIP, BREAK, AND, OR, NOT, INT, FLOAT, NUMBER, ID, STRING 
+    LTE, GTE, SKIP, BREAK, AND, OR, NOT, TINT, TFLOAT, NUMBER, INT, ID, LITERAL
   ]
 
   #literals
-  literals = { '(', ')', '{', '}', ';', ',' }
-
-  # ignore spaces and tabs
-  ignore = ' \t'
-
-  # ignore comments
-  ignore_comment = r'/\*.*\*/'
-
-  # count newlines
-  @_(r'\n+')
-  def ignore_newline(self, t):
-    self.lineno += t.value.count('\n')
-
-  # error handling
-  def error(self, t):
-    print(f'Illegal character {t.value[0]}')
-    self.index += 1
+  literals = { '(', ')', '{', '}', ';', ',', '[', ']'}
 
   #tokens declaration
   FUN = r'fun'
@@ -37,8 +23,8 @@ class LexerForPL0(Lexer):
   ELSE = r'else'
   WHILE = r'while'
   DO = r'do'
-  VAR = r':'
   ASSIGN = r':='
+  VARDECL = r':'
   PRINT = r'print'
   READ = r'read'
   WRITE = r'write'
@@ -58,46 +44,68 @@ class LexerForPL0(Lexer):
   AND = r'and'
   OR = r'or'
   NOT = r'not'
-  INT = r'int'
-  FLOAT = r'float'
-  STRING = r'".*"'
+  TINT = r'int'
+  TFLOAT = r'float'
 
-  NUMBER = r'\d+|\d+.\d+'
+  # integer
+  @_(r'\d+')
+  def INT(self, t):
+    t.value = int(t.value)
+    return t
 
-  # ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
-  ID = r'[a-zA-Z_]'
+  # float or int number
+  @_(r'\d+|\d+.\d+')
+  def NUMBER(self, t):
+    if '.' in t.value:
+      t.value = float(t.value)
+    else:
+      t.value = int(t.value)
+    return t
+  
+  # string literal
+  LITERAL = r'"(?:[^"\\]|\\["n\\])*"'
+
+  # identifier
+  ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
+
+    # ignore spaces and tabs
+  ignore = ' \t\r'
+
+  # ignore comments
+  ignore_comment = r'/\*.*\*/'
+
+  # count newlines
+  @_(r'\n+')
+  def ignore_newline(self, t):
+    self.lineno += t.value.count('\n')
+
+  # uncompleted comment
+  @_(r'/\*.*')
+  def ignore_uncompleted_comment(self, t):
+    print(f'Uncompleted comment at line {self.lineno}')
+    self.index += 1
+
+  # error handling
+  def error(self, t):
+    print(f'Illegal character {t.value[0]}')
+    self.index += 1
+
+def print_lexer(source):
+  #print tokens using rich library
+  lexer = LexerForPL0()
+  console = Console()
+  table = Table(title='Tokens')
+  table.add_column('Type')
+  table.add_column('Value')
+  table.add_column('Line', justify='right')
+  table.add_column('Index', justify='right')
+  table.add_column('End', justify='right')
+  for tok in lexer.tokenize(source):
+    table.add_row(tok.type, str(tok.value), str(tok.lineno), str(tok.index), str(tok.end))
+  console.print(table)
+  
 
 if __name__ == '__main__':
-  lexer = LexerForPL0()
-  text = '''
-    fun main()
-    v:int[8192];
-    i:int;
-    n:int;
-  begin
-    print("Entre n: ");
-    read(n);
-    i := 0;
-    while i < n do
-    begin
-      read(v[i]);
-      i := i+1
-    end;
-    quicksort(0, n-1, v);
-    i := 0;
-    while i < n-1 do
-    begin
-      write(v[i]); print(" ");
-      if 0 < v[i] - v[i+1] then
-      begin
-        print("Quicksort falló "); write(i); print("\n") ; return(0)
-      end
-      else
-        i := i+1
-    end;
-    write(v[i]);
-    print("Éxito\n")
-  end
-  '''
-  for tok in lexer.tokenize(text):
-    print(tok)
+  with open('test.pl0') as f:
+    text = f.read()
+  print_lexer(text)
