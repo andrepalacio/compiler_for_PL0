e61f43b8797f67027799869d65e9e34ff9f97a14
Andres Palacio <andres.palacio@utp.edu.co>
2023-09-29 21:40:49 -0500

    lexer for test

--- lexer.py
+++ lexer.py
@@ -1,18 +1,19 @@
 from sly import Lexer
 from rich.table import Table
 from rich.console import Console
+from sys import argv, exit
 
 class LexerForPL0(Lexer):
 
   # tokens
-  tokens = [
+  tokens = {
     FUN, BEGIN, END, IF, THEN, ELSE, WHILE, DO, VARDECL, ASSIGN, PRINT, 
     READ, WRITE, RETURN, PLUS, MINUS, TIMES, DIVIDE, EQ, NEQ, LT, GT,
     LTE, GTE, SKIP, BREAK, AND, OR, NOT, TINT, TFLOAT, INT, FLOAT, ID, STRING
-  ]
+  }
 
   #literals
-  literals = { '(', ')', '{', '}', ';', ',', '[', ']'}
+  literals = { '(', ')', '{', '}', ';', ',', '[', ']', '+', '-', '*', '/'}
 
   # ignore spaces and tabs
   ignore = ' \t\r'
@@ -29,7 +30,7 @@ class LexerForPL0(Lexer):
     return t
   
   # integer
-  @_(r'(([1-9]\d+)|0)(?![\de-])')
+  @_(r'(([1-9]\d*)|0)(?![\de-])')
   def INT(self, t):
     t.value = int(t.value)
     return t
@@ -37,41 +38,37 @@ class LexerForPL0(Lexer):
   # string
   STRING = r'"(?:[^"\\]|\\["n\\])*"'
 
-  # identifier
-  ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
-
   #tokens declaration
-  ID['fun'] = FUN
-  ID['begin'] = BEGIN
-  ID['end'] = END
-  ID['if'] = IF
-  ID['then'] = THEN
-  ID['else'] = ELSE
-  ID['while'] = WHILE
-  ID['do'] = DO
-  ID[':='] = ASSIGN
-  ID[':'] = VARDECL
-  ID['print'] = PRINT
-  ID['read'] = READ
-  ID['write'] = WRITE
-  ID['return'] = RETURN
-  ID['skip'] = SKIP
-  ID['break'] = BREAK
-  ID['and'] = AND
-  ID['or'] = OR
-  ID['not'] = NOT
-  ID['int'] = TINT
-  ID['float'] = TFLOAT
-  PLUS = r'\+'
-  MINUS = r'-'
-  TIMES = r'\*'
-  DIVIDE = r'/'
+  FUN = r'fun\b'
+  BEGIN = r'begin\b'
+  END = r'end\b'
+  IF = r'if\b'
+  THEN = r'then\b'
+  ELSE = r'else\b'
+  WHILE = r'while\b'
+  DO = r'do\b'
+  PRINT = r'print\b'
+  READ = r'read\b'
+  WRITE = r'write\b'
+  RETURN = r'return\b'
+  SKIP = r'skip\b'
+  BREAK = r'break\b'
+  AND = r'and\b'
+  OR = r'or\b'
+  NOT = r'not\b'
+  TINT = r'int\b'
+  TFLOAT = r'float\b'
+  ASSIGN = r':='
+  VARDECL = r':'
   EQ = r'=='
   NEQ = r'!='
   LT = r'<'
   GT = r'>'
   LTE = r'<='
   GTE = r'>='
+
+  # identifier
+  ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
 
   # count newlines
   @_(r'\n+')
@@ -85,10 +82,8 @@ class LexerForPL0(Lexer):
     self.index += 1
 
   # error handling
-  @_(r'.+')
   def error(self, t):
-    print(f'Illegal character {t.value} at line {t.lineno}')
-    self.lineno += 1
+    print(f'Illegal character {str(t.value[0])} at line {t.lineno}')
     self.index += 1
 
 def print_lexer(source):
@@ -107,7 +102,18 @@ def print_lexer(source):
   
 
 if __name__ == '__main__':
-  filePath = './test1/badnumbers.pl0'
-  with open(filePath) as f:
-    text = f.read()
-  print_lexer(text)
+  if len(argv) != 2:
+    print('Usage: python lexer.py <file>')
+    exit(1)
+
+  try:
+    filePath = argv[1]
+    with open(filePath) as f:
+      text = f.read()
+    print_lexer(text)
+  except FileNotFoundError:
+    print(f'File {filePath} not found')
+    exit(1)
+  except Exception as e:
+    print(f'Error {e} occurred while reading file {filePath}')
+    exit(1)
